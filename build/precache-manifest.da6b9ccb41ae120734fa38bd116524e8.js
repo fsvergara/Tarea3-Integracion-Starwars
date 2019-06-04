49770cce03976cdc3e4b5d19e3e76886d80faf3c
fsvergara <fsvergar@uc.cl>
2019-06-04 15:18:58 -0400

    initial commit

---
+++ build/precache-manifest.da6b9ccb41ae120734fa38bd116524e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb239c032242cd2f9cff9a55f137574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd3879fd74d0d8f67f2c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fd3879fd74d0d8f67f2c",
+    "url": "/static/js/main.e415c8e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
